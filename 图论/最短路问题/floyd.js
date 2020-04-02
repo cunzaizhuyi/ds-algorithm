27f44c889942fa8fd6b99f75b272f213a247c4b7
cunzaizhuyi <877824709@qq.com>
2020-04-02 08:37:17 +0800

    floyd

--- 图论/最短路问题/floyd.js
+++ 图论/最短路问题/floyd.js
@@ -1,8 +1,11 @@
+// 全名 floyd-warshall。两个人独立发表该算法
 
 // 用于求：多源最短路问题。复杂度O(n3)。
+// 可以处理带负权边的图，不能处理带负权环的图。
+// 有向图 无向图 都可以。
 let floyd = (adjMatrix) => {
     let k = adjMatrix.length; // 顶点数
-    // K轮松弛
+    // 多少顶点就进行多少轮松弛
     for(let i = 0; i < k; i++){
         for(let j = 0; j < adjMatrix.length; j++){
             for(let k = 0; k < adjMatrix.length; k++){
